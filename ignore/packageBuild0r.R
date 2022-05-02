10fe1d12831d6d1412b4878f25986ed2cdb5e507
Jeschull <fabian.jeschull@kit.edu>
2022-05-02 16:42:30 +0200

    update README.Rdm

--- ignore/packageBuild0r.R
+++ ignore/packageBuild0r.R
@@ -1,5 +1,10 @@
 usethis::use_tidy_description()
 usethis::use_build_ignore(c("myfolder", "file"))
 
+#load files in data folder
+usethis::use_data('filename')
+#call objects from data folder with
+data('filename')
+
 devtools::load_all()
 
